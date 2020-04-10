e6840b1aee72d85afd88d618ff0b6f2a2328f85e
ovesco <guillaume.hochet@gmail.com>
2020-04-10 22:36:36 +0200

    update function to reflect correct workflow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { HttpClient } = require('@actions/http-client');
 const pad0 = n => n < 10 ? `0${n}` : `${n}`;
 const getDayStamp = (date) => date.getFullYear() + '-' + pad0(date.getMonth() + 1) + '-' +  pad0(date.getDate());
 const numberOfDiagnostics = 6;
-const numberOfMetadataRows = 2;
+const numberOfMetadataRows = 4;
 const csvHeader = [
   'date',
   'postal_code',
@@ -19,13 +19,25 @@ const csvHeader = [
 
 const emptyDiagnosticRow = Array.from(Array(numberOfDiagnostics).keys()).map(() => 0);
 
+
 const httpClient = new HttpClient();
 const daystamp = getDayStamp(new Date());
+const yesterday = (d => new Date(d.setDate(d.getDate()-1)))(new Date);
+const yestedaystamp = getDayStamp(yesterday);
 const exportUrl = core.getInput('daily_export_json_url');
+const geolocationUrl = core.getInput('geo_locations_csv_url');
 const exportToken = core.getInput('daily_export_json_token');
 const mergedDatasetFilename = core.getInput('merged_dataset_name');
 const dailyReportName = core.getInput('daily_report_name').replace('{date}', daystamp);
 const dailyMergedReportName = core.getInput('daily_merged_report_name');
+const csvSeparator = core.getInput('csv_separator');
+const todayFilename = core.getInput('today_filename');
+
+const getGeocoding = async () => {
+  const response = await httpClient.get(geolocationUrl);
+  const rawData = await response.readBody();
+  return rawData.split('\n').map((it) => it.split(csvSeparator)).map(([a,b,c,d]) => [b,d,c]);
+};
 
 /**
  * Loads a CSV file and maps it to a multidimensional array
@@ -34,7 +46,7 @@ const dailyMergedReportName = core.getInput('daily_merged_report_name');
  */
 const loadCSVFile = (file) => {
   const rawData = fs.existsSync(file) ? fs.readFileSync(file, 'utf-8') : csvHeader.join(',');
-  return rawData.split('\n').filter(it => it.trim() !== '')
+  return rawData.split('\r\n').filter(it => it.trim() !== '')
       .map(it => it.split(','));
 };
 
@@ -43,69 +55,7 @@ const loadCSVFile = (file) => {
  * @param json
  * @returns {string}
  */
-const toCsv = (json) => json.map(it => it.join(',')).join('\n');
-
-/**
- * Updates the given dataset with the given daily changes
- * @param dailyChanges
- * @param dataset
- * @param indexPredicate used in order to find correct dataset row based on current daily change (current dataset item, daystamp, dailychange locator) => BOOLEAN
- * @param op used to decide what to do with previous dataset diagnostic count when adding current daily change, (current dataset value, daily change value) => INTEGER
- * @returns {*}
- */
-const dailyChangesUpdateDataset = (dailyChanges, dataset, indexPredicate, op) => {
-  dailyChanges.forEach(({ locator, daystamp, diagnostics }) => {
-    const rowIndex = dataset.findIndex((it) => indexPredicate(it, daystamp, locator));
-    const row = rowIndex === -1 ? [daystamp, locator, ...emptyDiagnosticRow] : dataset[rowIndex];
-
-    Object.entries(diagnostics).forEach(([diagnosticId, diagnosticCount]) => {
-      const did = numberOfMetadataRows + parseInt(diagnosticId, 10);
-      row[did] = op(parseInt(row[did], 10), diagnosticCount);
-    });
-
-    if (rowIndex === -1) dataset.push(row);
-    else dataset[rowIndex] = row;
-  });
-
-  return dataset;
-};
-
-/**
- * Will update merged changes CSV file with current daily changes
- * @param dailyChanges
- */
-const buildAllMergedChanges = async (dailyChanges) => {
-
-  const mergedChanges = loadCSVFile(`./${mergedDatasetFilename}`);
-  const updatedChanges = dailyChangesUpdateDataset(dailyChanges, mergedChanges,
-      (it, daystamp, locator) => it[0] === daystamp && it[1] === locator,
-      (previousValue, dailyChangeValue) => dailyChangeValue // We only keep daily change value as there's one row per postal code X daystamp
-  );
-  fs.writeFileSync(`./${mergedDatasetFilename}`, toCsv(updatedChanges), 'utf-8');
-};
-
-const buildDailyMergedChanges = async (dailyChanges) => {
-  const yesterday = (d => new Date(d.setDate(d.getDate()-1)))(new Date);
-  const yesterdayChanges = loadCSVFile(`./${dailyMergedReportName.replace('{date}', getDayStamp(yesterday))}`);
-  const todayChanges = yesterdayChanges.map(it => yesterdayChanges.indexOf(it) === 0 ? it : [daystamp, ...it.slice(1)]);
-
-  const updatedChanges = dailyChangesUpdateDataset(dailyChanges, todayChanges, (it, daystamp, locator) => it[1] === locator, (a, b) => a + b);
-  fs.writeFileSync(`./${dailyMergedReportName.replace('{date}', daystamp)}`, toCsv(updatedChanges), 'utf-8');
-};
-
-const buildDailyChanges = async (dailyChanges) => {
-  const changes = [csvHeader];
-  dailyChanges.forEach(({ locator, daystamp, diagnostics }) => {
-    const row = [daystamp, locator, ...emptyDiagnosticRow];
-    Object.entries(diagnostics).forEach(([diagnosticId, diagnosticCount]) => {
-      row[numberOfMetadataRows + parseInt(diagnosticId, 10)] += diagnosticCount;
-    });
-    changes.push(row);
-  });
-
-  const dailyChangeCsv = changes.map(it => it.join(',')).join('\n');
-  fs.writeFileSync(`./${dailyReportName}`, dailyChangeCsv, 'utf-8');
-};
+const toCsv = (json) => json.map(it => it.join(',')).join('\r\n');
 
 try {
   console.log('Starting daily aggregation at [' + new Date().toTimeString() + '] for [' + daystamp + ']');
@@ -114,15 +64,124 @@ try {
 
     const currentDailyChangesResponse = await httpClient.get(exportUrl + `?token=${exportToken}&date=${daystamp}`);
     const currentDailyChanges = JSON.parse(await currentDailyChangesResponse.readBody());
+    const geocoding = await getGeocoding();
+    const unknownNpas = loadCSVFile('./unknown-geocoding.csv');
 
+    const getLatLong = (locator) => {
+      const item = geocoding.find((it) => it[0] === locator);
+      if (item === undefined) {
+        if (!unknownNpas.includes(locator)) unknownNpas.push(locator);
+        return null;
+      }
+      return [item[1], item[2]];
+    };
+
+    /**
+     * Updates the given dataset with the given daily changes
+     * @param dailyChanges
+     * @param dataset
+     * @param indexPredicate used in order to find correct dataset row based on current daily change (current dataset item, daystamp, dailychange locator) => BOOLEAN
+     * @param op used to decide what to do with previous dataset diagnostic count when adding current daily change, (current dataset value, daily change value) => INTEGER
+     * @returns {*}
+     */
+    const dailyChangesUpdateDataset = (dailyChanges, dataset, indexPredicate, op) => {
+      dailyChanges.forEach(({ locator, daystamp, diagnostics }) => {
+        const rowIndex = dataset.findIndex((it) => indexPredicate(it, daystamp, locator));
+        const yesterdayRowIndex = dataset.findIndex((it) => indexPredicate(it, yestedaystamp, locator));
+        const latLong = getLatLong(locator);
+
+        // If this geocoding locator is known
+        if (latLong !== null) {
+          const row = yesterdayRowIndex === -1
+              ? [daystamp, locator, ...latLong, ...emptyDiagnosticRow]  // No data for yesteday create empty row
+              : [...dataset[yesterdayRowIndex]]; // Already data for yesterday reuse it
+          row[0] = daystamp;
+          Object.entries(diagnostics).forEach(([diagnosticId, diagnosticCount]) => {
+            let finalId = diagnosticId;
+            if (diagnosticId === '4') finalId = '5';
+            if (diagnosticId === '5') finalId = '4';
+            const did = numberOfMetadataRows + parseInt(finalId, 10);
+            row[did] = op(parseInt(row[did], 10), diagnosticCount);
+          });
+
+          if (rowIndex === -1) dataset.push(row);
+          else dataset[rowIndex] = row;
+        }
+      });
+
+      return dataset;
+    };
+
+    /**
+     * Will update merged changes CSV file with current daily changes
+     */
+    const buildAllMergedChanges = async () => {
+      const mergedChanges = loadCSVFile(`./${mergedDatasetFilename}`);
+      const npasRaw = mergedChanges.slice(1).map((it) => it[1]);
+      const npas = npasRaw.filter((v,i) => npasRaw.indexOf(v) === i);
+      const updatedChanges = dailyChangesUpdateDataset(currentDailyChanges, mergedChanges,
+          (it, daystamp, locator) => it[0] === daystamp && it[1] === locator,
+          (previousValue, dailyChangeValue) => previousValue + dailyChangeValue // We only keep daily change value as there's one row per postal code X daystamp
+      );
+
+      // Take back state from yesterday from all npa's that didn't change today
+      const npasPresent = currentDailyChanges.map((it) => it.locator);
+      const unchangedNpas = npas.filter((it) => !npasPresent.includes(it));
+
+      // Append state
+      const yesterdayStamp = getDayStamp(yesterday);
+      unchangedNpas.forEach((locator) => {
+        const row = updatedChanges.find((it) => it[0] === yesterdayStamp && it[1] === locator);
+
+        if (row !== undefined) {
+          const newRow = [...row];
+          newRow[0] = daystamp;
+
+          if (locator === '3966') {
+            console.log(row, newRow);
+          }
+          updatedChanges.push(newRow);
+        }
+      });
+      fs.writeFileSync(`./test-${mergedDatasetFilename}`, toCsv(updatedChanges), 'utf-8');
+    };
+
+    const buildDailyMergedChanges = async () => {
+      const yesterdayChanges = loadCSVFile(`./${dailyMergedReportName.replace('{date}', getDayStamp(yesterday))}`);
+      const todayChanges = yesterdayChanges.map(it => yesterdayChanges.indexOf(it) === 0 ? it : [daystamp, ...it.slice(1)]);
+
+      const updatedChanges = dailyChangesUpdateDataset(currentDailyChanges, todayChanges, (it, daystamp, locator) => it[1] === locator, (a, b) => a + b);
+      const filecontent = toCsv(updatedChanges);
+      fs.writeFileSync(`./${dailyMergedReportName.replace('{date}', daystamp)}`, filecontent, 'utf-8'); // write daily report
+      fs.writeFileSync(`./${todayFilename}`, filecontent); // write today file
+    };
+
+    const buildDailyChanges = async () => {
+      const changes = [csvHeader];
+      currentDailyChanges.forEach(({ locator, daystamp, diagnostics }) => {
+        const row = [daystamp, locator, ...emptyDiagnosticRow];
+        Object.entries(diagnostics).forEach(([diagnosticId, diagnosticCount]) => {
+          row[numberOfMetadataRows + parseInt(diagnosticId, 10)] += diagnosticCount;
+        });
+        changes.push(row);
+      });
+
+      const dailyChangeCsv = changes.map(it => it.join(',')).join('\n');
+      fs.writeFileSync(`./${dailyReportName}`, dailyChangeCsv, 'utf-8');
+    };
+
+
+    /**
+     * Running scripts one by one to avoid running out of memory
+     */
     console.log('Building new merged changes file...');
-    await buildAllMergedChanges(currentDailyChanges);
+    await buildAllMergedChanges();
 
     console.log('Building daily changes file...');
-    await buildDailyChanges(currentDailyChanges);
+    await buildDailyChanges();
 
     console.log('Building merged daily changes file...');
-    await buildDailyMergedChanges(currentDailyChanges);
+    await buildDailyMergedChanges();
 
     console.log('Done');
   })();
